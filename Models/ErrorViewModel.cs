bf625c508a6fc3c06e84df16ec2a2aa805cab07c
Hafız <ramizovhafiz@gmail.com>
2025-04-07 19:52:42 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BeyleqanSebeti.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
